15f24b2030043db3989c29dce45328c53969684d
user <user@FB04>
2016-12-16 09:58:26 +0800

    OOP-HW7

---
+++ src/fcu/iecs/oop/pokemon/Bulbasaur.java
@@ -0,0 +1,13 @@
+package fcu.iecs.oop.pokemon;
+
+public class Bulbasaur extends Pokemon{
+	Bulbasaur(String name,PokemonType type,int cp)
+	{
+		super(name,type,cp);
+	}
+	public void attack()
+	{
+		System.out.println("Tackle...");
+	}
+
+}
---
+++ src/fcu/iecs/oop/pokemon/Charmander.java
@@ -0,0 +1,13 @@
+package fcu.iecs.oop.pokemon;
+
+public class Charmander extends Pokemon{
+	Charmander(String name,PokemonType type,int cp)
+	{
+		super(name,type,cp);
+	}
+	public void attack()
+	{
+		System.out.println("Ember...");
+	}
+
+}
